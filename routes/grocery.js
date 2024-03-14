c662c477edee2bdf8fbd6f2668bc55ba109389a6
MarcelCronje <126375480+MarcelCronje@users.noreply.github.com>
2024-03-14 23:33:28 +0200

    Routes-Comments

--- routes/grocery.js
+++ routes/grocery.js
@@ -1,7 +1,13 @@
+// adds express in this file
 const express = require('express');
 
+// calls the router in a variable
 const router = express.Router();
 
+// get request
+// this is where we are going to do something if we het that endpoint 
 router.get('/', );
 
+// we will need to export this to index.js file
+// wich contains the get request and its controller
 module.exports = router;
